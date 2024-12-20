8367a71ebb3552b7ab3984d9e5d8f772cec2bbfa
JunHyun <bjsd2001a@naver.com>
2024-12-20 16:27:58 +0900

    클래스 다양성

---
+++ src/study/inherit/inherit04/v2/Main01.java
@@ -0,0 +1,68 @@
+package study.inherit.inherit04.v2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cleaner c1 = new Cleaner();
+		Cleaner c2 = new DustCleaner();
+		Cleaner c3 = new WindowCleaner();
+		Cleaner c4 = new FloorCleaner();
+		
+		List<Cleaner> list = new ArrayList<Cleaner>();
+		list.add(new DustCleaner());
+		list.add(new DustCleaner());
+		list.add(new DustCleaner());
+		list.add(new WindowCleaner());
+		list.add(new WindowCleaner());
+		list.add(new WindowCleaner());
+		list.add(new FloorCleaner());
+		list.add(new FloorCleaner());
+		list.add(new FloorCleaner());
+		
+		c1.removeDust();
+		DustCleaner dc = new DustCleaner();
+		dc.removeDust();
+		
+		for(Cleaner c : list) {
+			c.removeDust();
+			c.washWindow();
+			c.cleanFloor();
+		}
+		
+		
+// v1 에서 가져옴
+//		DustCleaner[] dustArr = new DustCleaner[3];
+//		dustArr[0] = new DustCleaner();
+//		dustArr[1] = new DustCleaner();
+//		dustArr[2] = new DustCleaner();
+//				
+//		List<WindowCleaner> windowList = new ArrayList<WindowCleaner>();
+//		windowList.add(new WindowCleaner());
+//		windowList.add(new WindowCleaner());
+//		windowList.add(new WindowCleaner());
+//		
+//		List<FloorCleaner> floorList = new ArrayList<FloorCleaner>();
+//		floorList.add(new FloorCleaner());
+//		floorList.add(new FloorCleaner());
+//		floorList.add(new FloorCleaner());
+		
+//		for(int i=0; i<dustArr.length; i++) {
+//			dustArr[i].removeDust();
+//		}
+//		
+//		for(int i=0; i<windowList.size(); i++) {
+//			windowList.get(i).washWindow();
+//		}
+//		
+//		for(FloorCleaner fc : floorList) {
+//			fc.cleanFloor();
+//		}
+		
+	}
+
+}
---
+++ src/study/inherit/inherit04/v3/Cleaner.java
@@ -0,0 +1,20 @@
+package study.inherit.inherit04.v3;
+
+public class Cleaner {
+	
+	public void doClean() {
+		System.out.println("Cleaner doClean()");
+	}
+	
+//	public void removeDust() {
+//		System.out.println("Cleaner 먼지털이");
+//	}
+//	
+//	public void cleanFloor() {
+//		System.out.println("Cleaner 바닥 쓸기");
+//	}
+//	
+//	public void washWindow() {
+//		System.out.println("Cleaner 창문 닦기");
+//	}
+}
